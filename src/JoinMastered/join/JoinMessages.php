c766aa5a2f7bc52ab6e5c788521c31eb8b3473a4
FreeGamingHere <freegaminghere@gmail.com>
2018-06-15 01:07:36 +0300

    Update JoinMessages.php

--- src/JoinMastered/join/JoinMessages.php
+++ src/JoinMastered/join/JoinMessages.php
@@ -6,10 +6,10 @@ namespace JoinMastered\JoinMessages
 
 use JoinMastered\src\Main
 
-use pocketmine/event/Listener;
-use pocketmine/event/PLayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
-class JoinMastered\join\JoinMessages implements listner{
+class JoinMastered implements listner{
 
     public function onJoin(PlayerJoinEvent $event) : void{
         $player = $event->getPlayer();
@@ -18,11 +18,11 @@ class JoinMastered\join\JoinMessages implements listner{
         $player->sendMessage("§e- §3Twitter: §e@§5Cosmos§9PE");
         $player->sendMessage("§e- §3Store: §5cosmospe1§9.§5buycraft§9.§5net");
         $player->sendMessage("§e- §3Discord: <>");
-        $player->sendMessage("§e- §3Features:: §cFactions");
+        $player->sendMessage("§e- §3Features: §cFactions");
         $player->sendMessage("§€");
         $player->sendMessage("§e- §6Make Sure to Read are Rules before playing!");
         $player->sendMessage("§e- §6Voting earns you a free voting key!");
-        $player->sendMessage("§e- §6Donating Rewards you with cool Perks a/an abilites);
+        $player->sendMessage("§e- §6Donating Rewards you with cool Perks a/an abilites");
         $player->sendMessage("§€");
     }
 }
