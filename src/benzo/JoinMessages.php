e4f3c725ec3e88fd181df8220f556a4910243d8c
FreeGamingHere <freegaminghere@gmail.com>
2018-06-15 01:43:35 +0300

    Create JoinMessages.php

---
+++ src/benzo/JoinMessages.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace benzo;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class JoinMessages implements Listener{
+
+    public function onJoin(PlayerJoinEvent $event) : void{
+        $player = $event->getPlayer();
+        $player->sendMessage("§€");
+        $player->sendMessage("§l§6» §aWelcome To §5Cosmos§9PE §c!");
+        $player->sendMessage("§e- §3Twitter: §e@§5Cosmos§9PE");
+        $player->sendMessage("§e- §3Store: §5cosmospe1§9.§5buycraft§9.§5net");
+        $player->sendMessage("§e- §3Discord: <>");
+        $player->sendMessage("§e- §3Features: §cFactions");
+        $player->sendMessage("§€");
+        $player->sendMessage("§e- §6Make Sure to Read are Rules before playing!");
+        $player->sendMessage("§e- §6Voting earns you a free voting key!");
+        $player->sendMessage("§e- §6Donating Rewards you with cool Perks a/an abilites");
+        $player->sendMessage("§€");
+    }
+}
