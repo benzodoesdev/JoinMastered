9c364db703a18ad0e2b1ec28d00dcb755348c4da
benzodoesdev <33678572+benzodoesdev@users.noreply.github.com>
2018-06-14 04:44:55 -0500

    fixed i believe

---
+++ src/Main.php
@@ -0,0 +1,29 @@
+
+<?php
+
+
+
+declare(strict_types=1);
+
+
+
+namespace JoinMastered;
+
+
+use JoinMastered\join\JoinMessage;
+use JoinMastered\join\JoinTitle;
+
+
+
+class Main extends PluginBase{
+
+
+    /** @var JoinMessage $joinmessage */
+
+    private $joinmessage;
+
+    /** @var JoinTitle $jointitle */
+
+    private $jointitle;
+
+}
