888d4891a1f8e70ea0a6f182a3a486df715640bd
FreeGamingHere <freegaminghere@gmail.com>
2018-06-15 02:28:54 +0300

    Update Main.php

--- src/benzo/Main.php
+++ src/benzo/Main.php
@@ -4,13 +4,9 @@ namespace benzo;
 
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase {
 
-    /** @var JoinMessage $joinmessage */
-    private $joinmessage;
-    /** @var JoinTitle $jointitle */
-    private $jointitle;
-    public function onEnable(){
+    public function onEnable(): void {
         $this->getServer()->getPluginManager()->registerEvents(new JoinMessage($this), $this);
         $this->getServer()->getPluginManager()->registerEvents(new JoinTitle($this), $this);
     }
